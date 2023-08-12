4fd9486ef451f63aeda1e8774d3acbbe075b0ad2
Kalpana <52186147+kalpana0795@users.noreply.github.com>
2023-08-12 09:15:51 +0530

    Add Initial Configurations and Dependecies (#1)

---
+++ lib/omniauth-avaza.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'omniauth/avaza/version'
+require 'omniauth/strategies/avaza'
---
+++ lib/omniauth/strategies/avaza.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'omniauth/strategies/oauth2'
+
+module OmniAuth
+  module Strategies
+    class Avaza < OmniAuth::Strategies::OAuth2
+    end
+  end
+end
